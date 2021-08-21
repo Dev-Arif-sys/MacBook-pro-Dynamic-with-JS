a48958a8fab2d0c10eb957e3b00450f0661db048
Arif <arifp57895698@gmail.com>
2021-08-21 13:42:44 +0600

    indentation done

--- js/app.js
+++ js/app.js
@@ -1,71 +1,62 @@
+let belowTotal=document.getElementById("below-total");
 
-function updatingChipCost(existPriceId,price){
+// updating total price section
+function updatingCostTotal(existPriceId,price){
+    // updating chipcost
      let chipExistingPrice=document.getElementById(existPriceId);
      chipExistingPrice.innerText=price
-     updatingTotalPrice();
-}
-
-function gettingChipCost(costId){
-    var chipCost=document.getElementById(costId).innerText;
-    chipCost=parseFloat(chipCost);
-    return chipCost;
-}
-
-let belowTotal=document.getElementById("below-total");
-function updatingTotalPrice(){
-     let bestPrice=gettingChipCost("best-price");
-     let memoryCost=gettingChipCost("memory-cost");
-     let storageCost=gettingChipCost("storage-cost");
-     let deliveryCharge=gettingChipCost("delivery-charge");
-
+   
+    //  updating total price and below total
+     let bestPrice=gettingElementToNum("best-price");
+     let memoryCost=gettingElementToNum("memory-cost");
+     let storageCost=gettingElementToNum("storage-cost");
+     let deliveryCharge=gettingElementToNum("delivery-charge");
      let total= bestPrice+memoryCost+storageCost+deliveryCharge;
-     
      document.getElementById("total-price").innerText=total;  
      belowTotal.innerText=total;
+}
+// getting element to numbeer
+function gettingElementToNum(elementId){
+    var element=document.getElementById(elementId).innerText;
+    elementNum=parseFloat(element);
+    return elementNum;
 }
 
 
 // memory cost interaction
-
-document.getElementById("memory-8gb").addEventListener("click",function(){
-                   
-    updatingChipCost("memory-cost",0)
+document.getElementById("memory-8gb").addEventListener("click",function(){                  
+    updatingCostTotal("memory-cost",0)
 }) 
-
-document.getElementById("memory-16gb").addEventListener("click",function(){
-                   
-    updatingChipCost("memory-cost",180)
+document.getElementById("memory-16gb").addEventListener("click",function(){             
+    updatingCostTotal("memory-cost",180)
 }) 
 
 
 // storage cost interaction
 document.getElementById("ssd-256gb").addEventListener("click",function(){                 
-    updatingChipCost("storage-cost",0)
+    updatingCostTotal("storage-cost",0)
 }) 
-
 document.getElementById("ssd-512gb").addEventListener("click",function(){                 
-    updatingChipCost("storage-cost",100)
+    updatingCostTotal("storage-cost",100)
 }) 
-
 document.getElementById("ssd-1tb").addEventListener("click",function(){                 
-    updatingChipCost("storage-cost",180)
+    updatingCostTotal("storage-cost",180)
 }) 
 
 
 // delivery charge interaction
 document.getElementById("charge-free").addEventListener("click",function(){                 
-    updatingChipCost("delivery-charge",0)
+    updatingCostTotal("delivery-charge",0)
 }) 
-
 document.getElementById("charge-20").addEventListener("click",function(){                 
-    updatingChipCost("delivery-charge",20)
+    updatingCostTotal("delivery-charge",20)
 }) 
 
 
 // updating below total Section
 document.getElementById("coupon-btn").addEventListener("click",function(){
     let couponInput=document.getElementById("coupon-input");
-    let totalPrice=gettingChipCost("total-price")
+    let totalPrice=gettingElementToNum("total-price")
            if(couponInput.value=="stevekaku"){
                let couponDiscount=(totalPrice/100)*20;
                belowTotal.innerText=totalPrice-couponDiscount;
